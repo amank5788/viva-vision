3fb9ada787414f2cd73ebb4c3d0e2b4083c9677a
amank5788 <88306289+amank5788@users.noreply.github.com>
2024-02-19 21:05:17 +0530

    frontend setup

---
+++ frontend/background.js
@@ -0,0 +1,98 @@
+console.log("Background!!");
+
+let chats = {}; // { meetId: [ { ticId, role, content } ] }
+let chatGptResponses = {}; // { meetId: { generatedResponse, startChat, endChat } }
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  // if request is of type CURRENT_CHAT then concat on the ticId
+  if (request.type === "CURRENT_CHAT") {
+    console.log("CURRENT_CHAT", request.data.chat)
+    const meetId = request.data.meetId;
+    const passedChat = request.data.chat;
+    if (!chats[meetId]) {
+      chats[meetId] = [];
+    }
+    const chat = chats[meetId];
+    passedChat.forEach((passedChatInstance) => {
+      const chatIndex = chat.findIndex(
+        (chatInstance) => chatInstance.ticId === passedChatInstance.ticId
+      );
+      if (chatIndex > -1) {
+        chat[chatIndex].content = passedChatInstance.content;
+      } else {
+        chat.push(passedChatInstance);
+      }
+    });
+    chrome.runtime.sendMessage({ type: "FULL_CURRENT_CHATS", data: { chats } });
+  }
+  if (request.type === "GET_RESPONSE") {
+    const { meetId, startChat, endChat } = request.data;
+    // get the slice of chat from startChat to endChat and make a deep copy of it
+    const chat = chats[meetId];
+    const chatSlice = JSON.parse(
+      JSON.stringify(chat.slice(startChat, endChat + 1))
+    );
+    // preprocess the chatSlice so that any recurring roles are merged into one
+    const preprocessedChatSlice = [];
+    let i = 0;
+    let currentChatInstance = null;
+    while (i < chatSlice.length) {
+      currentChatInstance = chatSlice[i];
+      const nextChatInstance = chatSlice[i + 1];
+      if (
+        nextChatInstance &&
+        currentChatInstance.role === nextChatInstance.role
+      ) {
+        currentChatInstance.content += " " + nextChatInstance.content;
+        chatSlice.splice(i + 1, 1);
+      } else {
+        delete currentChatInstance.ticId;
+        preprocessedChatSlice.push(currentChatInstance);
+        i++;
+      }
+    }
+
+    console.log("preprocessedChatSlice", preprocessedChatSlice)
+
+    // send the preprocessedChatSlice to the backend https://635f-68-234-232-23.ngrok.io/infer_response and pass the preprocessedChatSlice as the json body
+    const response = await fetch(
+      `http://localhost:2300/response`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(preprocessedChatSlice),
+      }
+    );
+    const responseJson = await response.json();
+    console.log("responseJson", responseJson)
+    const generatedResponse = responseJson;
+    
+    // if response fails then send a message for FAILED_RESPONSE
+    if (!generatedResponse) {
+      chrome.runtime.sendMessage({
+        type: "FAILED_RESPONSE"
+      });
+      return;
+    } else {
+      // send the generatedResponse back to the popup
+      chatGptResponses[meetId] = { generatedResponse, startChat, endChat };
+      chrome.runtime.sendMessage({
+        type: "CHATGPT_RESPONSE",
+        data: chatGptResponses[meetId],
+      });
+    }
+
+  }
+  // if request is of type GET_RESPONSE then send the response back to the popup
+  if (request.type === "GET_CHATGPT_RESPONSE") {
+    const { meetId } = request.data;
+    if (chatGptResponses[meetId]) {
+      chrome.runtime.sendMessage({
+        type: "CHATGPT_RESPONSE",
+        data: chatGptResponses[meetId],
+      });
+    }
+  }
+});
---
+++ frontend/popup/popup.js
@@ -0,0 +1,162 @@
+console.log("Popup!!");
+
+let chat = [];
+let startChat = null;
+let endChat = null;
+
+// get element of id selectedIndexes
+const startContentEx = document.getElementById("startContentEx");
+const endContentEx = document.getElementById("endContentEx");
+const responseStartContentEx = document.getElementById(
+  "responseStartContentEx"
+);
+const responseEndContentEx = document.getElementById("responseEndContentEx");
+const responseContainer = document.getElementById("responseContainer");
+
+// check the current tab url
+let meetId = null;
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  const currentTab = tabs[0];
+  const url = currentTab.url;
+  meetId = url.split("?")[0].split("/").pop();
+});
+
+const reactToSelectedChats = () => {
+  // remove the class selectedChatStart and selectedChatEnd from all the divs
+  const chatInstances = document.querySelectorAll(
+    ".youChatInstance, .interviewerChatInstance"
+  );
+  chatInstances.forEach((chatInstance) => {
+    chatInstance.classList.remove("selectedChatStart");
+    chatInstance.classList.remove("selectedChatEnd");
+    generateResponseBtn.classList.remove("bg-green-600");
+    generateResponseBtn.classList.add("bg-red-600");
+  });
+  // find the startChat and endChat divs and add the class selectedChatStart and selectedChatEnd respectively
+  if (startChat !== null) {
+    const startChatInstance = document.querySelector(
+      `[ticid="${chat[startChat].ticId}"]`
+    );
+    startChatInstance.classList.add("selectedChatStart");
+  }
+  if (endChat !== null) {
+    const endChatInstance = document.querySelector(
+      `[ticid="${chat[endChat].ticId}"]`
+    );
+    endChatInstance.classList.add("selectedChatEnd");
+  }
+
+  startContentEx.innerText = startChat !== null ? chat[startChat].content : "";
+  endContentEx.innerText = endChat !== null ? chat[endChat].content : "";
+
+  if (startChat !== null && endChat !== null) {
+    generateResponseBtn.disabled = false;
+    generateResponseBtn.classList.remove("bg-red-600");
+    generateResponseBtn.classList.add("bg-green-600");
+
+
+  } else {
+    generateResponseBtn.disabled = true;
+    generateResponseBtn.classList.add("bg-red-600");
+  }
+};
+
+const generateResponseBtn = document.getElementById("gernerateResponse");
+// add event listener to the button of id gernerateResponse
+generateResponseBtn.addEventListener("click", function () {
+  // send a message to the service worker of type "GENERATE_RESPONSE" with the startChat and endChat
+  chrome.runtime.sendMessage({
+    type: "GET_RESPONSE",
+    data: { meetId, startChat, endChat },
+  });
+  startChat = null;
+  endChat = null;
+  reactToSelectedChats();
+});
+
+const selectChatInstance = (targetChatInstanceElement) => {
+  // get ticId attribute from the targetChatInstanceElement
+  const targetTicId = targetChatInstanceElement.getAttribute("ticid");
+  const chatIndex = chat.findIndex(
+    (chatInstance) => chatInstance.ticId === targetTicId
+  );
+  if (startChat === null) {
+    startChat = chatIndex;
+  } else if (endChat === null) {
+    if (chatIndex < startChat) {
+      startChat = chatIndex;
+    } else {
+      endChat = chatIndex;
+    }
+  } else {
+    startChat = null;
+    endChat = null;
+  }
+
+  reactToSelectedChats();
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (
+    request.type === "FULL_CURRENT_CHATS" &&
+    meetId &&
+    request.data.chats[meetId]
+  ) {
+    // iterate through the chat and populate the chatContainer div with divs containing the class youChatInstance and interviewerChatInstance depending on the role, the text content of the div should be the content
+    const chatContainer = document.getElementById("chatContainer");
+    // only add a new div if the ticId is not already in the chatContainer
+    chat = request.data.chats[meetId];
+
+    // if there is nothing inside the responseContainer.innerText then send a message to the service worker of type "GET_CHATGPT_RESPONSE"
+    if (responseContainer.innerText.includes("(Response will show here)")) {
+      chrome.runtime.sendMessage({
+        type: "GET_CHATGPT_RESPONSE",
+        data: { meetId },
+      });
+    }
+
+    chat.forEach((chatInstance) => {
+      const chatDiv = document.createElement("div");
+    
+      chatDiv.innerHTML =`<p style="color: rgb(253 224 71); font-weight: 600">${chatInstance.role}</p>
+                        <p>${chatInstance.content}</p>`
+      
+      if (chatInstance.role.toUpperCase() === "YOU") {
+        chatDiv.classList.add("youChatInstance");
+      } else {
+        chatDiv.classList.add("interviewerChatInstance");
+      }
+      // add an attribute ticId with the value of the ticId
+      chatDiv.setAttribute("ticId", chatInstance.ticId);
+
+      chatDiv.addEventListener("click", function () {
+        selectChatInstance(chatDiv);
+      });
+
+      if (document.querySelector(`[ticId="${chatInstance.ticId}"]`) === null) {
+        chatContainer.appendChild(chatDiv);
+      } else if (chatInstance.ticId) {
+        // get the div with the ticId and update the innerText
+        const existingChatDiv = document.querySelector(
+          `[ticId="${chatInstance.ticId}"]`
+        );
+        existingChatDiv.innerHTML = `<p style="color: rgb(253 224 71); font-weight: 600" >${chatInstance.role}</p>
+        <p>${chatInstance.content}</p>`;
+      }
+    });
+  }
+  if (request.type === "CHATGPT_RESPONSE") {
+    responseStartContentEx.innerText = chat[request.data.startChat].content;
+    responseEndContentEx.innerText = chat[request.data.endChat].content;
+    responseContainer.innerHTML = request.data.generatedResponse;
+  }
+  if (request.type === "FAILED_RESPONSE") {
+    responseStartContentEx.innerText = "";
+    responseEndContentEx.innerText = "";
+    responseContainer.innerText = "(Sorry response failed to generate please rejoin the meet and try again)";
+  }
+});
+
+
+
+
