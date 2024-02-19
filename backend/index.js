a5c545b7a01a00debf5e45f97dd10d73cbe2281c
amank5788 <88306289+amank5788@users.noreply.github.com>
2024-02-19 21:02:31 +0530

    till gemini set up

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,72 @@
 const express=require ('express');
 const app=express();
-const port=2300;
-const OpenAI=require ("openai");
+const cors=require('cors');
+const body_parser=require('body-parser');
+require('dotenv').config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const port=process.env.PORT;
+const markdown=require('markdown-js');
 
-const openai = new OpenAI();
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
+const API=process.env.API_KEY;
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(API);
 
-  console.log(completion.choices[0]);
+// ...
+app.use(cors());
+app.use(body_parser.json());
+async function run(question) {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  const prompt = question;
+  try{
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    const html = markdown.makeHtml(text);
+   
+    return html;
+  }
+  catch(e){
+    console.log("error while responding by gemini",e);
+  }
+
+  
 }
 
-main();
+app.post("/response",async(req,res)=>{
+  var response="";
 
+  try{
+    for(var i=0;i<req.body.length;i++){
+      response+=`<strong>${req.body[i].role} :-</strong>`;
+      const resp= await run((req.body[i].content));
+      response+=resp;
+      console.log(req.body[i].role,resp);
+     
+    }
+    res.json(response);
+  }
+  catch(e){
+    console.log("error while talking to gemini api",e);
+  }
+ 
+      
+    
+    
+  
+  
+ 
+  
+ 
+  
+})
 
+//run("what is object oriented programming");
 
 app.get('/',(req,res)=>{
-    res.send('hello');
+    res.send('welcome to voice-vision');
 })
 
 
